8a1443e722aab04cc02c32638fb3c64d1befe1a6
Alex Harrison <Alex.Harrison@derivco.co.za>
2018-01-19 19:03:00 +0200

    resolved webGL errors, particles not rendering though

--- dist-PHASER/neutrinoparticles.phaser.js
+++ dist-PHASER/neutrinoparticles.phaser.js
@@ -71,7 +71,8 @@ class PIXINeutrinoMaterials {
   }
 
   setup(pMatrix, scale) {
-    var gl = this.gl;
+   // console.log('PIXINeutrinoMaterials setup', pMatrix)
+    // var gl = this.gl;
 
     this.pMatrix = pMatrix;
     this.scale = scale.slice();
@@ -79,24 +80,18 @@ class PIXINeutrinoMaterials {
   }
 
   switchToNormal(renderer) {
-    var gl = this.gl;
-
     this._setProgram(this.shaderProgram);
-    renderer.state.setBlendMode(0);
+    renderer.blendModeManager.setBlendMode(0);
   }
 
   switchToAdd(renderer) {
-    var gl = this.gl;
-
     this._setProgram(this.shaderProgram);
-    renderer.state.setBlendMode(1);
+    renderer.blendModeManager.setBlendMode(1);
   }
 
   switchToMultiply(renderer) {
-    var gl = this.gl;
-
     this._setProgram(this.shaderProgramMultiply);
-    renderer.state.setBlendMode(2);
+    renderer.blendModeManager.setBlendMode(2);
   }
 
   _setProgram(program) {
@@ -104,6 +99,7 @@ class PIXINeutrinoMaterials {
 
     if (program != this.currentProgram) {
       gl.useProgram(program);
+      // console.log('_setProgram',program.pMatrixUniform, this.pMatrix)
       gl.uniformMatrix3fv(program.pMatrixUniform, false, this.pMatrix);
       gl.uniform1i(program.samplerUniform, 0);
       gl.uniform2f(program.scaleUniform, this.scale[0], this.scale[1]);
@@ -333,11 +329,12 @@ class PIXINeutrinoEffectModel extends Phaser.Sprite {
     this.numTexturesToLoadLeft = -1;
     this.texturesRemap = null;
 
+    this.onReady = new Phaser.Signal();
+
     var pixiNeutrinoEffect = this;
     this.ctx.neutrino.loadEffect(this.ctx.effectsBasePath + effectPath, function (effectModel) {
       pixiNeutrinoEffect._onEffectLoaded(effectModel);
     });
-    this.onReady = new Phaser.Signal();
   }
 
   ready() {
@@ -456,6 +453,9 @@ class PIXINeutrinoEffect extends Phaser.Group {
   constructor(effectModel, position, game, rotation, scale) {
     super(game, null);
 
+    this._renderCanvas = this.renderCanvas;
+    this._renderWebGL = this.renderWebGL;
+
     this.ctx = effectModel.ctx;
     this.effectModel = effectModel;
     this.effect = null;
@@ -512,32 +512,72 @@ class PIXINeutrinoEffect extends Phaser.Group {
 
     var gl = renderer.gl;
 
+    //TODO - need a Phaser compatible way to reset stuff as per below
+    /*
     renderer.setObjectRenderer(renderer.emptyRenderer);
     renderer.bindVao(null);
     renderer.state.resetAttributes();
 
     renderer.state.push();
-    renderer.state.setState(renderer.state.defaultState);
+    renderer.state.setState(renderer.state.defaultState);*/
 
     // hack! the only way to discard current shader for futher engine rendering
-    renderer._activeShader = null;
+    //renderer._activeShader = null;
 
-    var target = renderer._activeRenderTarget;
+    //---- from pixi.js filterManager ------------
+    // update projection
+    // now restore the regular shader..
+    // this.renderSession.shaderManager.setShader(this.defaultShader);
+    //gl.uniform2f(this.defaultShader.projectionVector, filterArea.width/2, -filterArea.height/2);
+    //gl.uniform2f(this.defaultShader.offsetVector, -filterArea.x, -filterArea.y);
+    // -----------------------------------
+    const defaultShader = game.renderer.shaderManager.defaultShader;
+    game.renderer.shaderManager.setShader(defaultShader);
 
-    this.ctx.materials.setup(target.projectionMatrix.toArray(true), [this.scale.x, this.scale.y]);
+    const renderSession = game.renderer.renderSession;
+    const projection = renderSession.projection,
+      offset = renderSession.offset;
+    gl.uniform2f(defaultShader.projectionVector, projection.x, -projection.y);
+    gl.uniform2f(defaultShader.offsetVector, -offset.x, -offset.y);
+
+    // - _activeRenderTarget doesn't exist in this version of pixi
+    // var target = renderer._activeRenderTarget;
+    //console.log('projectionVector', defaultShader.projectionVector, 'offsetVector', defaultShader.offsetVector)
+    //projectionMatrix doesn't exist in this version of pixi
+
+    /* Matrix.ToArray
+    array[0] = this.a; > x scale
+    array[1] = this.c; > x skew
+    array[2] = this.tx;
+    array[3] = this.b; > y skew
+    array[4] = this.d; > y scale
+    array[5] = this.ty;
+    array[6] = 0;
+    array[7] = 0;
+    array[8] = 1;
+     */
+    const projectionMatrix = [1, 0, 0, 1, 0, 0, 0, 0, 1];
+    //TODO - set transform and scale values appropriately
+
+
+    // this.ctx.materials.setup(target.projectionMatrix.toArray(true), [this.scale.x, this.scale.y]);
+    this.ctx.materials.setup(projectionMatrix, [this.scale.x, this.scale.y]);
 
     this.effect.fillGeometryBuffers([1, 0, 0], [0, -1, 0], [0, 0, -1]);
 
     this.renderBuffers.updateGlBuffers();
     this.renderBuffers.bind();
 
-    for (var renderCallIdx = 0; renderCallIdx < this.renderBuffers.numRenderCalls; ++renderCallIdx) {
-      var renderCall = this.renderBuffers.renderCalls[renderCallIdx];
-      var texIndex = this.effect.model.renderStyles[renderCall.renderStyleIndex].textureIndices[0];
+    for (let renderCallIdx = 0; renderCallIdx < this.renderBuffers.numRenderCalls; ++renderCallIdx) {
+      const renderCall = this.renderBuffers.renderCalls[renderCallIdx];
+      const texIndex = this.effect.model.renderStyles[renderCall.renderStyleIndex].textureIndices[0];
 
-      renderer.bindTexture(this.effectModel.textures[texIndex], 0, true);
+      // - bindTexture doesn't exist in this version of pixi
+      //try updateTexture instead
+      // renderer.bindTexture(this.effectModel.textures[texIndex], 0, true);
+      game.renderer.updateTexture(this.effectModel.textures[texIndex]);//, 0, true);
 
-      var materialIndex = this.effect.model.renderStyles[renderCall.renderStyleIndex].materialIndex;
+      const materialIndex = this.effect.model.renderStyles[renderCall.renderStyleIndex].materialIndex;
       switch (this.effect.model.materials[materialIndex]) {
         default: this.ctx.materials.switchToNormal(renderer); break;
         case 1: this.ctx.materials.switchToAdd(renderer); break;
@@ -547,7 +587,7 @@ class PIXINeutrinoEffect extends Phaser.Group {
       gl.drawElements(gl.TRIANGLES, renderCall.numIndices, gl.UNSIGNED_SHORT, renderCall.startIndex * 2);
     }
 
-    renderer.state.pop();
+    //renderer.state.pop();
   }
 
   restart(position, rotation) {
@@ -589,6 +629,7 @@ class PIXINeutrinoEffect extends Phaser.Group {
   }
 
   _onEffectReady() {
+    console.log('Effect ready')
     var position = [this.position.x / this.scale.x, this.position.y / this.scale.y, this.positionZ / this.scaleZ];
     var rotation = this.ctx.neutrino.axisangle2quat_([0, 0, 1], this.rotation % 360);
 
