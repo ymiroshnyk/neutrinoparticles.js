643bfd49a17d2014089024db2d7224222e4f20c6
ALEX-HP\Alex <harrison_alex@hotmail.com>
2018-02-16 15:22:43 +0200

    dont close mainWindow between tests, this breaks on windows

--- test/Phaser/main.js
+++ test/Phaser/main.js
@@ -50,12 +50,10 @@ function start(){
 
   ipcMain.on('test-result', (event, data) => {
     logOutput(data);
-    console.log('setting timeout...')
     setTimeout(e => {
-      console.log('timeout called to activate test!')
       activateTest();
     }, 500);
-    mainWindow.close();
+    if(_testQueue.length === 0) mainWindow.close();
   });
 
   ipcMain.on('fetch-settings', (event, data) => {
@@ -137,6 +135,7 @@ function logOutput(data){
 }
 
 function shutdown(){
+  console.log('shutdown');
   if(mainWindow) mainWindow.close();
   app.quit();
 }
@@ -193,21 +192,21 @@ function _parse(value){
 app.on('ready', start)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+// app.on('window-all-closed', function () {
+//   // On OS X it is common for applications and their menu bar
+//   // to stay active until the user quits explicitly with Cmd + Q
+//   if (process.platform !== 'darwin') {
+//     app.quit()
+//   }
+// })
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+// app.on('activate', function () {
+//   // On OS X it's common to re-create a window in the app when the
+//   // dock icon is clicked and there are no other windows open.
+//   if (mainWindow === null) {
+//     createWindow()
+//   }
+// })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
