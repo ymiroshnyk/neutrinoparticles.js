a114640e770fa489fb3cc60dc4fde447fac64b18
Yurii Miroshnyk <ext-yurii.miroshnyk@here.com>
2018-03-02 13:13:03 +0200

    Removed old textures for pixi. Adjusted scripts for testing webgl and canvas.

--- test/run-params-ref.sh
+++ test/run-params-ref.sh
@@ -2,4 +2,5 @@
 FRAMEWORK=${BASH_ARGV[1]}
 PARAMS=$(cat ${BASH_ARGV[0]})
 cd ..
-npm run test-$FRAMEWORK $PARAMS reference_pass=1
+npm run test-$FRAMEWORK $PARAMS webgl=0 reference_pass=1
+npm run test-$FRAMEWORK $PARAMS webgl=1 reference_pass=1
--- test/run-params.sh
+++ test/run-params.sh
@@ -2,4 +2,6 @@
 FRAMEWORK=${BASH_ARGV[1]}
 PARAMS=$(cat ${BASH_ARGV[0]})
 cd ..
-npm run test-$FRAMEWORK $PARAMS
+npm run test-$FRAMEWORK $PARAMS webgl=0
+npm run test-$FRAMEWORK $PARAMS webgl=1
+
