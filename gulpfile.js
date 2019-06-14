41034908ca3ecdb59846542c87e80402ec72dc74
Oleksandr Kysil <oleksandr.Kysil@netent.com>
2019-06-14 09:48:13 +0300

    [build] added umd and regular bundling.

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,90 @@
 const gulp = require("gulp"),
-  concat = require("gulp-concat"),
-  sourcemaps = require("gulp-sourcemaps"),
-  babel = require("gulp-babel"),
-  watch = require('gulp-watch');
+    concat = require("gulp-concat"),
+    sourcemaps = require("gulp-sourcemaps"),
+    babel = require("gulp-babel"),
+    watch = require('gulp-watch'),
+    wrapper = require("gulp-module-wrapper"),
+    rename = require("gulp-rename"),
+    mainDist = "./dist/neutrinoparticles.js",
+    outputDistFile = "neutrinoparticles",
+    bundleSuffix = ".bundle",
+    umdSuffix = ".umd",
+    jsFileExtension = ".js";
+
 
 const tasks = [
-  {
-    name: "phaser",
-    sourcesMask: "./src/Phaser/**/*.js",
-    distPath: "./dist-PHASER/",
-    outputDistFile: "neutrinoparticles.phaser.js"
-  },
-  {
-    name: "pixi",
-    sourcesMask: "./src/PIXI/**/*.js",
-    distPath: "./dist-PIXI/",
-    outputDistFile: "neutrinoparticles.pixi.js"
-  }
+    {
+        name: "phaser",
+        sourcesMask: "./src/Phaser/**/*.js",
+        distPath: "./dist-PHASER/"
+    },
+    {
+        name: "pixi",
+        sourcesMask: "./src/PIXI/**/*.js",
+        distPath: "./dist-PIXI/"
+    }
 ];
+
+const getDistFileName = function({name}){
+    return  outputDistFile +"."+ name + jsFileExtension;
+};
+
+const getDistBundleFileName = function({name}){
+    return  outputDistFile +"."+ name + bundleSuffix + jsFileExtension;
+};
+
+const getDistUmdFileName = function({name}){
+    return  outputDistFile +"."+ name + umdSuffix + jsFileExtension;
+};
+
+const getDistUmdBundleFileName = function({name}){
+    return  outputDistFile +"."+ name + umdSuffix + bundleSuffix + jsFileExtension;
+};
 
 tasks.forEach(function (task) {
 
-  // build task
-  gulp.task("build-" + task.name, function() {
-    return gulp.src(task.sourcesMask, {base: task.distPath})
-      .pipe(sourcemaps.init())
-      .pipe(concat(task.outputDistFile))
-      .pipe(babel())
-      .pipe(sourcemaps.write("."))
-      // .pipe(watch(task.sourcePath + "*.js"))
-      .pipe(gulp.dest(task.distPath));
-  });
+    // build task
+    gulp.task("build-" + task.name, function () {
+        return gulp.src(task.sourcesMask, {base: task.distPath})
+            .pipe(sourcemaps.init())
+            .pipe(concat(getDistFileName(task)))
+            .pipe(babel())
+            .pipe(sourcemaps.write("."))
+            // .pipe(watch(task.sourcePath + "*.js"))
+            .pipe(gulp.dest(task.distPath));
+    });
 
-  // watch task
-  gulp.task("watch-" + task.name, function () {
-    return watch(task.sourcesMask, function (data) {
-      gulp.run("build-" + task.name);
-    })
-  });
+    gulp.task("wrap-umd-" + task.name, ["build-" + task.name], function () {
+        return gulp.src(task.distPath + getDistFileName(task))
+            .pipe(wrapper(
+                {
+                    type: 'umd'
+                }
+            ))
+            .pipe(concat(getDistUmdFileName(task)))
+            .pipe(gulp.dest(task.distPath));
+    });
+
+    gulp.task("bundle-umd-" + task.name, ["wrap-umd-" + task.name], function () {
+        return gulp.src([mainDist, task.distPath + getDistUmdFileName(task)])
+            .pipe(concat(getDistUmdBundleFileName(task)))
+            .pipe(gulp.dest(task.distPath));
+
+    });
+
+    gulp.task("bundle-" + task.name, ["build-" + task.name], function () {
+        return gulp.src([mainDist, task.distPath + getDistFileName(task)])
+            .pipe(concat(getDistBundleFileName(task)))
+            .pipe(gulp.dest(task.distPath));
+    });
+
+    gulp.task("do-all-" + task.name, [ "bundle-umd-" + task.name, "bundle-" + task.name]);
+
+    // watch task
+    gulp.task("watch-" + task.name, function () {
+        return watch(task.sourcesMask, function (data) {
+            gulp.run("build-" + task.name);
+        })
+    });
 
 });
